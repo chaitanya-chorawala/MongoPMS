38f8b2e21a49d2ba638f516aa9594222e83bf379
chaitanya-chorawala <chorawala.chaitanya@gmail.com>
2021-11-28 19:41:47 +0530

    Initial commit

---
+++ Routers/Product.js
@@ -0,0 +1,24 @@
+require("dotenv").config();
+const express = require("express");
+const router = express.Router();
+router.use(express.json());
+
+const mongoose = require("mongoose");
+console.log(process.env.MONOGURL);
+// mongoose
+//     .connect((process.env.MONOGURL)
+//     .then(()=>console.log("Mongo connected!"));
+
+const ProductModel = require("../Models/Product");
+
+router.get("/", (req, res) => res.send("Product router"));
+
+//Add product
+router.post("/Add", (req, res) => {
+    const { product } = req.body;
+    ProductModel.create(product);
+
+    return res.json({data : "Product Added!"});
+});
+
+module.exports = router;
